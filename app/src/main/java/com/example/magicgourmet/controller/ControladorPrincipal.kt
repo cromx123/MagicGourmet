c7a05ae2e1097ffae41d816903db8f6e4beeae9f
cromx123 <73903821+cromx123@users.noreply.github.com>
2024-08-14 22:24:46 -0400

    Se modificaron los controladoradores, quedaron ocupables

--- app/src/main/java/com/example/magicgourmet/controller/ControladorPrincipal.kt
+++ app/src/main/java/com/example/magicgourmet/controller/ControladorPrincipal.kt
@@ -1,14 +1,34 @@
 package com.example.magicgourmet.controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import com.example.magicgourmet.R
 
 class ControladorPrincipal : ComponentActivity() {
+    private lateinit var btnhome: Button
+    private lateinit var btnbuscar: Button
+    private lateinit var btncrear: Button
+    private lateinit var btnperfil: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.vista_principal)
 
+        btnbuscar.setOnClickListener {
+            val optionsIntent = Intent(this, ControladorBuscarReceta::class.java)
+            startActivity(optionsIntent)
+        }
+        btncrear.setOnClickListener {
+            val optionsIntent = Intent(this, ControladorAnadirReceta::class.java)
+            startActivity(optionsIntent)
+        }
+        btnperfil.setOnClickListener {
+            val optionsIntent = Intent(this, ControladorVisFavorito::class.java)
+            startActivity(optionsIntent)
+        }
     }
 }
                                                                         
