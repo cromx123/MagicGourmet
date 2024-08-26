a55fa484cf45f4f84027451ea9ce2167e0a15026
cromx123 <73903821+cromx123@users.noreply.github.com>
2024-08-26 03:24:19 -0400

    Se actualizan las funciones de busquedas

--- app/src/main/java/com/example/magicgourmet/controller/ControladorVisFavorito.kt
+++ app/src/main/java/com/example/magicgourmet/controller/ControladorVisFavorito.kt
@@ -4,16 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.magicgourmet.R
+import com.example.magicgourmet.adapter.RecetaAdapter
+import com.example.magicgourmet.model.DatabaseHelper
 
 
 class ControladorVisFavorito: ComponentActivity() {
+    private lateinit var dbHelper: DatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.vista_vis_favorito)
 
         val btnperfilper =findViewById<ImageButton>(R.id.btnperfilper)
         btnperfilper.setImageResource(R.drawable.perfil_us_selec)
+
+        dbHelper = DatabaseHelper(this)
+        val recetasConPasos = dbHelper.obtenerRecetasConPasos()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerviewFavoritos)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = RecetaAdapter(this,recetasConPasos)
+
 
         menuInferiorButtons()
     }
